a25e2e35b086d1b137c6731157c9002fe8df2e5b
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-12-24 11:27:47 +0100

    chore: initial commit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './main.css'
+
+import TestHeading from './components/TestHeading.vue'
+import TestButton from './components/TestButton.vue'
+
+export { TestButton, TestHeading }
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), tailwindcss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'TestingComponents',
+      fileName: 'testing-components',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        },
+      },
+    },
+  },
+})
