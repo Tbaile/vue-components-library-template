c1e78ab8fcb1459f721c6a9c20d60ee6c2bd3fd8
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2024-12-25 18:20:49 +0100

    chore: initial commit

---
+++ src/components/TestButton.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { loading = false } = defineProps<{
+  loading?: boolean
+}>()
+</script>
+
+<template>
+  <button
+    :disabled="loading"
+    class="disabled:cursor-not-allowed"
+  >
+    <slot />
+  </button>
+</template>
