96a3f31870d908efda85809c297e976c5d1fd1a3
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2024-12-27 19:24:55 +0100

    build: added more storybook plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-themes'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-themes',
+    'storybook-addon-pseudo-states',
+    '@storybook/addon-a11y',
+  ],
   framework: {
     name: '@storybook/vue3-vite',
     options: {
