d05a6155c49bf6c83d17fc1f92c029d3cb82ff22
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2024-12-27 13:02:22 +0100

    build: added dark mode for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,16 @@
-import type { Preview } from '@storybook/vue3'
+import type { Preview, VueRenderer as Renderer } from '@storybook/vue3'
+import { withThemeByClassName } from '@storybook/addon-themes'
 
-const preview: Preview = { }
+const preview: Preview = {
+  decorators: [
+    withThemeByClassName<Renderer>({
+      themes: {
+        light: ' ',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+}
 
 export default preview
