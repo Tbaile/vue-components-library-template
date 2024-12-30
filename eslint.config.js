9aba42f4ec80fad3b8ee992283b43a6026c92c8c
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2024-12-30 14:54:59 +0100

    build: removed analysis of storybook

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/storybook-static/**'],
   },
 
   ...pluginVue.configs['flat/recommended'],
