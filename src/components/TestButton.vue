95ff49eae8f29c661d087a257b43a294aa6b3094
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2025-01-03 01:36:10 +0100

    feat: added poppins as example and updated tailwind configuration

--- src/components/TestButton.vue
+++ src/components/TestButton.vue
@@ -8,7 +8,10 @@ const { loading = false } = defineProps<{
 </script>
 
 <template>
-  <button :disabled="loading" class="disabled:cursor-not-allowed">
+  <button
+    :disabled="loading"
+    class="bg-primary hover:bg-primary-hover ring-primary-focus rounded-md px-3 py-2 text-sm text-white shadow-sm ring-offset-2 focus:ring-2 disabled:cursor-not-allowed disabled:opacity-50 dark:text-[#030712]"
+  >
     <slot />
   </button>
 </template>
